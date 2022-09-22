d4d60058355c6d8f28b761566c8fd93fd1bf6b34
sikethedev <ivoinestrachan242@gmail.com>
2022-09-22 10:46:04 -0400

    adding google auth n github auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react";
 import Navbar from '../components/Navbar'
 
-function MyApp({ Component, pageProps }: AppProps) {
+
+function MyApp({ Component, pageProps: { ...pageProps } }: AppProps) {
   return (
-    <>
+    <SessionProvider>
     <Navbar />
   <Component {...pageProps} />
-  </>
+  </SessionProvider>
   )
 }
 
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,21 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
 
+export default NextAuth({
+  secret: process.env.UNITEAUTH_SECRET,
+  providers: [
+  
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    
+    
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+  ],
 
-
-
+})
