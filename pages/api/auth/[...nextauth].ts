ae899d0a0d538188ea416598753376abac24d007
sikethedev <ivoinestrachan242@gmail.com>
2022-10-03 10:42:51 -0400

    username

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import prisma from "../../../lib/prisma";
 
 export const authOptions: NextAuthOptions ={
    secret: process.env.UNITEAUTH_SECRET,
@@ -15,7 +16,33 @@ export const authOptions: NextAuthOptions ={
       clientSecret: process.env.GOOGLE_SECRET as string,
     }),
   ],
-
+  events: {
+    async createUser({ user }) {
+      let username;
+      if (user.name) {
+        username = user.name.replace(/\s/g, "").toLowerCase();
+      } else if (user.email) {
+        username = user.email.split("@")[0];
+      } else {
+        username = user.id;
+      }
+      try {
+        await prisma.user.update({
+          where: { id: user.id },
+          data: {
+            username,
+          },
+        });
+      } catch (e) {
+        username += Math.floor(1000 + Math.random() * 9000).toString();
+        await prisma.user.update({
+          where: { id: user.id },
+          data: {
+            username,
+          },
+        });
+      }
+    }}
 
 }
 
