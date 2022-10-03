e2c01ecd805fee40347323fdefcf3576cb167107
sikethedev <ivoinestrachan242@gmail.com>
2022-10-03 09:03:18 -0400

    prisma

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,12 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../../../lib/prisma";
 
 export const authOptions: NextAuthOptions ={
    secret: process.env.UNITEAUTH_SECRET,
+   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
@@ -15,6 +18,15 @@ export const authOptions: NextAuthOptions ={
       clientSecret: process.env.GOOGLE_SECRET as string,
     }),
   ],
+
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.id = user.id;
+      session.user.username = user.username as string;
+      return session;
+    },
+
+  }
 
 }
 
