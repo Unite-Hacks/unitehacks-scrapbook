90c80d3a783dbdc58652d1d1f402ba90c544e226
sikethedev <ivoinestrachan242@gmail.com>
2022-10-04 21:10:55 -0400

    credits

--- components/PostGrid.tsx
+++ components/PostGrid.tsx
@@ -10,6 +10,15 @@ export const PostGrid = ({children}: {children: ReactNode}) => {
 
     {children}
     </div>
+    <div className="text-center text-2xl">
+      Credits:
+      <a href="https://github.com/sikethedev">
+      <p className="text-blue-700 hover:underline ">@ivoine</p>
+      </a>
+      <a href="https://github.com/maggie-j-liu ">
+      <p className="text-blue-700 hover:underline ">@maggie</p>
+      </a>
+    </div>
     </>
   );
 }
